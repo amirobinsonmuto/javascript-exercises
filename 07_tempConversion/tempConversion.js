59af5fa4ae1fe2a07d6b8772dda1af44e45c2988
AmiRobinsonMuto <amimuto1002@gmail.com>
2022-07-16 12:56:42 -0700

    Init commit

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(a) {
+  let number = (a-32)*(5/9);
+  return Math.round(number * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(a) {
+  let number = a*(9/5)+32;
+  return Math.round(number * 10) / 10;
 };
 
 // Do not edit below this line
