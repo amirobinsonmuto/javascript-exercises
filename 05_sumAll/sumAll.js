59bd39dd27ee0883da617ac65f233478df1ab29d
AmiRobinsonMuto <amimuto1002@gmail.com>
2022-07-16 09:08:46 -0700

    Init Commit

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,16 @@
-const sumAll = function() {
 
+
+let sum = 0;
+
+const sumAll = function(a,b) {
+
+    for (let i = a; i <= b; i++) {
+      sum += i;
+    }
+
+    return sum;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
