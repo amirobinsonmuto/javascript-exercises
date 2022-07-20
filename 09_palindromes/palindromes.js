e1a814edf205cb68b776a9140ac3d8d479718c25
AmiRobinsonMuto <amimuto1002@gmail.com>
2022-07-19 18:57:09 -0700

    Init commit

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
 
+    let clean = str.toLowerCase().replace(/[!,. ]/g,'');
+
+    let result = true;
+
+    for (let i=0; i<=clean.length/2-1; i++) {
+        if(clean.substr(i, 1) !== clean.substr(-(++i), 1)) {
+           result = false;
+        } 
+    }
+
+    return result;
 };
 
 // Do not edit below this line
