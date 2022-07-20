6949f4dd02f2ad0092b714635b13a8cfeb4fb31d
AmiRobinsonMuto <amimuto1002@gmail.com>
2022-07-19 21:39:19 -0700

    Init commit

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
 
+    if (a <= 0) {
+        return 'OOPS';
+    } else {
+
+        let total = 1;
+        let num1 = 0;
+        let num2 = 1;
+
+        for (let i=1; i<a; i++) {
+
+            total = num1 + num2;
+            num1 = num2;
+            num2 = total;
+
+        }
+
+        return total;
+    }
 };
+
+
 
 // Do not edit below this line
 module.exports = fibonacci;
