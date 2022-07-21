ee71c04e86a896ba463b4f0cf73ca1062cc7b3a9
AmiRobinsonMuto <amimuto1002@gmail.com>
2022-07-21 10:09:11 -0700

    Init commit

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,25 @@
 const getTheTitles = function() {
 
-};
+    const books = [
+      {
+        title: 'Book',
+        author: 'Name'
+      },
+      {
+        title: 'Book2',
+        author: 'Name2'
+      }
+    ]
+  
+    let titleArr = [];
+  
+    for (let i=0; i<books.length; i++) {
+      titleArr.push(books[i]["title"]);
+    }
+  
+    return titleArr;
+  
+  };
 
 // Do not edit below this line
 module.exports = getTheTitles;
