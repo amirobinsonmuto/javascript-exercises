c2c6bf59e1a37959f65dd2d1bb4f567b5d9c86ce
AmiRobinsonMuto <amimuto1002@gmail.com>
2022-07-22 17:49:08 -0700

    Init commit

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
-};
+    let result = people.reduce((accumulator, person) => {
+
+      if (person.yearOfDeath === undefined) {
+        person.yearOfDeath = new Date().getFullYear();
+      }
+     
+      let age = person.yearOfDeath - person.yearOfBirth;
+
+      if (age > accumulator) {
+        accumulator = age;
+      } 
+
+      return accumulator;
+
+    }, 0);
+
+    return people.find(element => element.yearOfDeath - element.yearOfBirth === result);
+
+  }
 
 // Do not edit below this line
 module.exports = findTheOldest;
